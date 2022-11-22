f1a010674b635ae35ab638a5804625c3fb74feec
Wael Walid <wael.walid91@gmail.com>
2022-11-22 03:09:35 +0200

    adding logs and cron manager

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import express from 'express';
 import config from 'config';
 import morgan from 'morgan';
 import { createExpressServer, useContainer } from 'routing-controllers';
-
 import { Container } from 'typedi';
 import { AppDataSource } from './utility/data-source';
-// import { AppDataSource } from './utility/data-source';
+import { JobManager } from './jobs/CronManager';
+import { logger } from './utility/logger';
 
 useContainer(Container);
 require('dotenv').config();
@@ -14,78 +14,37 @@ require('dotenv').config();
 const port = config.get<number>('port');
 
 const app = createExpressServer({
-  controllers: [`${__dirname}/controllers/*.ts`],
-  middlewares: [`${__dirname}/middlewares/*.ts`],
+  controllers: [`${__dirname}/controllers/*.{ts,js}`],
+  middlewares: [`${__dirname}/middleware/*.{ts,js}`],
 });
 
-AppDataSource.initialize().then(() => {
-  // 1. Body parser
-  app.use(express.json({ limit: '10kb' }));
+try {
+  AppDataSource.initialize().then(() => {
+    // Body parser
+    app.use(express.json({ limit: '10kb' }));
 
-  // 2. Logger
-  if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+    // Logger
 
-  app.listen(port);
-  console.log(`Server started on port: ${port}`);
-}).catch((e: any) => {
+    app.use(morgan(
+      'request-id :id :remote-addr  :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"',
+      {
+        stream: {
+          write: (message) => {
+            logger.info(message.trim());
+          },
+        },
+      },
+    ));
+
+    app.listen(port);
+    console.log(`Server started on port: ${port}`);
+
+    // initiate cron jobs
+    const cron = new JobManager();
+    cron.run();
+  }).catch((e: any) => {
+    console.log(e);
+  });
+} catch (e) {
   console.log(e);
-});
-
-// UNHANDLED ROUTE
-// app.all('*', (req: Request, res: Response, next: NextFunction) => {
-//   next(new AppError(404, `Route ${req.originalUrl} not found`));
-// });
-
-// // GLOBAL ERROR HANDLER
-// app.use(
-//   (error: AppError, req: Request, res: Response, next: NextFunction) => {
-//     error.status = error.status || 'error';
-//     error.statusCode = error.statusCode || 500;
-
-//     res.status(error.statusCode).json({
-//       status: error.status,
-//       message: error.message,
-//     });
-//   },
-// );
-
-// AppDataSource.initialize()
-//   .then(async () => {
-//     const app = express();
-
-//     // 1. Body parser
-//     app.use(express.json({ limit: '10kb' }));
-
-//     // 2. Logger
-//     if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-
-//     // ROUTES
-//     app.use('/', (req: Request, res: Response) => {
-//       res.status(200).json({
-//         status: 'success',
-//         message: 'Welcome to Node.js, we are happy to see you',
-//       });
-//     });
-
-//     // UNHANDLED ROUTE
-//     // app.all('*', (req: Request, res: Response, next: NextFunction) => {
-//     //   next(new AppError(404, `Route ${req.originalUrl} not found`));
-//     // });
-
-//     // // GLOBAL ERROR HANDLER
-//     // app.use(
-//     //   (error: AppError, req: Request, res: Response, next: NextFunction) => {
-//     //     error.status = error.status || 'error';
-//     //     error.statusCode = error.statusCode || 500;
-
-//     //     res.status(error.statusCode).json({
-//     //       status: error.status,
-//     //       message: error.message,
-//     //     });
-//     //   },
-//     // );
-
-//     app.listen(5000);
-//     console.log(`Server started on port: ${port}`);
-//   })
-//   .catch((error) => console.log(error));
+}
